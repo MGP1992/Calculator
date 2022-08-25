8bc0f6cf491a249d5d61384cce30c029638e024e
MGP1992 <mathewgparkinson@gmail.com>
2022-08-25 15:18:17 +0100

    add operate function that takes numbers and operater to use

--- index.js
+++ index.js
@@ -24,6 +24,26 @@ function multiply(num1,num2) {
   return (num1 * num2);    
 };
 
+function operate(num1,num2,operator) {
+  switch (operator) {
+    case '+':
+      return add(num1,num2);
+      
+    case '-':
+      return subtract(num1,num2);
+      
+    case '/':
+      return divide(num1,num2);
+      b
+    case '*':
+      return multiply(num1,num2);
+      
+    default: throw new Error("Invalid operator")
+  }
+};
+
+ 
+
 
 const keys = Array.from(document.querySelectorAll('.btn'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
