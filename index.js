f077dc663f0a5b63d018c84ae1fe79da614b8d07
MGP1992 <mathewgparkinson@gmail.com>
2022-08-25 14:53:39 +0100

    add 4 basic operator functions

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ function pressKey(e) {
     key.classList.add('pressed');
 }
 
-  
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('pressed');
   }
+
+function add(num1,num2) {
+  return (num1 + num2);    
+};
+
+function subtract(num1,num2) {
+  return (num1 - num2);   
+};
+
+function divide(num1,num2) {
+  return (num1 / num2);  
+};
+
+function multiply(num1,num2) {
+  return (num1 * num2);    
+};
+
 
 const keys = Array.from(document.querySelectorAll('.btn'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
