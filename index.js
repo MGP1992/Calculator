f25b53e5a9a0639c25a422f119c8aaf6d3e4a383
MGP1992 <mathewgparkinson@gmail.com>
2022-08-25 20:07:00 +0100

    add delete and clear functions

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const keys = Array.from(document.querySelectorAll('.btn'));
+const calculatorDisplay = document.querySelector('.screendisplay')
+
 function pressKey(e) {
     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
     key.classList.add('pressed');
+    console.log(e.key);
+    if (e.key  == 'c'){
+      calculatorDisplay.textContent = "";
+    }
+    else if (e.key == 'Delete' ) { 
+      deleteLast();
+    
+    }
+    
+    else {updateDisplay(e.key);}
+    
 }
 
 function removeTransition(e) {
@@ -42,10 +56,23 @@ function operate(num1,num2,operator) {
   }
 };
 
+function updateDisplay (number) {
+  calculatorDisplay.textContent += number;
+  calculatorDisplay.classList.add("screendisplay");
+}
+
+function deleteLast() {
+  let string = calculatorDisplay.textContent;
+  let newString = string.substring(0, string.length - 1);
+  calculatorDisplay.textContent = newString;
+}
+
+
  
 
-
-const keys = Array.from(document.querySelectorAll('.btn'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 keys.forEach(key => key.addEventListener('click', (e) => e.target.classList.add('pressed')));
 window.addEventListener('keydown', pressKey);
+
+
+
