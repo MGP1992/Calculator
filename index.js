0422fab9085ea70de0be0b19ff066b9361d7e43d
MGP1992 <mathewgparkinson@gmail.com>
2022-08-26 11:10:11 +0100

    add calculateSum function (single operators only)

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const calculatorDisplay = document.querySelector('.screendisplay')
 function pressKey(e) {
     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
     key.classList.add('pressed');
-    console.log(e.key);
     if (e.key  == 'c'){
       calculatorDisplay.textContent = "";
-    }
-    else if (e.key == 'Delete' ) { 
+    }else if (e.key == 'Delete' ) { 
       deleteLast();
-    
-    }
-    
-    else {updateDisplay(e.key);}
+    }else if (e.key == '='){
+      calculateSum();  
+    }else {updateDisplay(e.key);}
     
 }
 
@@ -23,37 +20,19 @@ function removeTransition(e) {
   }
 
 function add(num1,num2) {
-  return (num1 + num2);    
+  return ((parseFloat(num1) + parseFloat(num2)));    
 };
 
 function subtract(num1,num2) {
-  return (num1 - num2);   
+  return (parseFloat(num1 - num2));   
 };
 
 function divide(num1,num2) {
-  return (num1 / num2);  
+  return (parseFloat(num1 / num2));  
 };
 
 function multiply(num1,num2) {
-  return (num1 * num2);    
-};
-
-function operate(num1,num2,operator) {
-  switch (operator) {
-    case '+':
-      return add(num1,num2);
-      
-    case '-':
-      return subtract(num1,num2);
-      
-    case '/':
-      return divide(num1,num2);
-      b
-    case '*':
-      return multiply(num1,num2);
-      
-    default: throw new Error("Invalid operator")
-  }
+  return (parseFloat(num1 * num2));    
 };
 
 function updateDisplay (number) {
@@ -62,10 +41,33 @@ function updateDisplay (number) {
 }
 
 function deleteLast() {
-  let string = calculatorDisplay.textContent;
-  let newString = string.substring(0, string.length - 1);
-  calculatorDisplay.textContent = newString;
+  let currentDisplay = calculatorDisplay.textContent;
+  let updatedDisplay = currentDisplay.substring(0, currentDisplay.length - 1);
+  calculatorDisplay.textContent = updatedDisplay;
 }
+
+function calculateSum() {
+  let currentDisplay = calculatorDisplay.textContent;
+  let newDisplay;
+
+  for (let i = 0; i < currentDisplay.length; i++) {
+    let valueBeforeOperator = currentDisplay.substring(0,i)
+    let valueAfterOperator = currentDisplay.substring(i+1)
+    if(currentDisplay[i] === '*') {
+      newDisplay = multiply(valueBeforeOperator,valueAfterOperator)
+      calculatorDisplay.textContent = newDisplay;
+    }else if(currentDisplay[i] === '/') {
+      newDisplay = divide(valueBeforeOperator,valueAfterOperator)
+      calculatorDisplay.textContent = newDisplay;
+    }else if(currentDisplay[i] === '+') {
+      newDisplay = add(valueBeforeOperator,valueAfterOperator)
+      calculatorDisplay.textContent = newDisplay;
+    }else if(currentDisplay[i] === '-') {
+      newDisplay = subtract(valueBeforeOperator,valueAfterOperator)
+      calculatorDisplay.textContent = newDisplay;
+    }
+  };
+};
 
 
  
